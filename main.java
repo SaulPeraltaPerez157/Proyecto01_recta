0263804738ed32cd0e759a0d7766b86fe7b7521a
Pioo157 <saulperalta01@gmail.com>
2023-11-17 16:56:14 -0600

    Segundo commit en la rama Recta_noir.
    -Agregando funcion tabla_positiva a la clase recta.

--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 import Recta.recta;
 
 public class main{
-		public static void main(String args[]){
-			System.out.println("Esto funciona");
-		}
+	public static void main(String args[]){
+		recta R1 = new recta(1,1);
+		System.out.println("Esto funciona");
+		System.out.println("Pendiente de la recta R1: " + R1.getm());
+		System.out.println("Ordenada al origen de la recta R1: " + R1.getb());
+		R1.tabla_positivo(1,4);
+	}
 }
