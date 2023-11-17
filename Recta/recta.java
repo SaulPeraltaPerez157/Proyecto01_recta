2cfc9ace28c0c087a0bf54a24c77f2f0a3937ca4
Pioo157 <saulperalta01@gmail.com>
2023-11-17 15:55:24 -0600

    Primer commit en la rama Recta_noir.
    -Agregando la clase recta.

---
+++ Recta/recta.java
@@ -0,0 +1,14 @@
+package Recta;
+public class recta{
+	private int m;
+	private int b;
+	public recta(int m, int b){
+		this.m = m;
+		this.b = b;
+	}
+
+	public int getm(){
+		return this.m;
+	}
+}
+
--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import Recta.recta;
+
 public class main{
 		public static void main(String args[]){
 			System.out.println("Esto funciona");
