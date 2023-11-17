bcc3bd13cf175cfdc2a0c5f2bf1c05bdfb887737
Pioo157 <saulperalta01@gmail.com>
2023-11-17 16:59:22 -0600

    Tercer commit en la rama Recta_noir.
    -Corrigiendo archivos.

--- Recta/recta.java
+++ Recta/recta.java
@@ -10,5 +10,22 @@ public class recta{
 	public int getm(){
 		return this.m;
 	}
+
+	public int getb(){
+		return this.b;
+	}
+
+	public void tabla_positivo(int value_i, int value_f){
+		if(value_i <= 0){
+			System.out.println("Ingrese un valor mayor a 0");
+		}
+		else{
+			System.out.println("----X---- |---- Y ----");
+			for(int i=value_i; i<value_f;i++){
+				System.out.print(i + "                ");
+				System.out.println(m*b+i);
+			}
+		}
+	}
 }
 
